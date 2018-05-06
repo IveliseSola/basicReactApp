77987745b5050d5513eccd441702c890cb96eff7
Ivelise Sola <solaivelise@yahoo.com>
2018-05-06 13:26:24 -0400

    UI colors

--- clicky/src/components/Wrapper/Wrapper.js
+++ clicky/src/components/Wrapper/Wrapper.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Wrapper.css";
 
-const Wrapper = props => <div className=" text-white bg-secondary wrapper">{props.children}</div>;
+const Wrapper = props => <div className=" bg-light  wrapper">{props.children}</div>;
 
 export default Wrapper;

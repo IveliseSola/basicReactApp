c2229ec061ddebfdb70827f771bc6b7e645e6111
Ivelise Sola <solaivelise@yahoo.com>
2018-05-06 13:32:19 -0400

    UI  Typography

--- clicky/src/components/Jumbotron/Jumbotron.js
+++ clicky/src/components/Jumbotron/Jumbotron.js
@@ -3,10 +3,10 @@ import "./Jumbotron.css";
 
 const Jumbotron = props => (
 <div className="jumbotron">
-  <h1 className="display-3">Clicky Disney Villains Game</h1>
+  <h1 className="display-3">Clicky Villains Game</h1>
   <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
   {/* <hr className="my-4"> */}
-  <p> Message: {props.children} </p>
+  <h5> Message: {props.children} </h5>
 </div>
 );
 
--- clicky/src/components/Nav/Nav.js
+++ clicky/src/components/Nav/Nav.js
@@ -3,7 +3,7 @@ import "./Nav.css";
 
 const Nav = props => (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Clicky Game</a>
+    <a className="navbar-brand" href="#">CVG</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
